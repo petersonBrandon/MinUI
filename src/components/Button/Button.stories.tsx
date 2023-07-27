d10b595a23e832bb393dd3b6c6b594cfa3bb5d9b
Brandon Peterson <brandonacfuntech@gmail.com>
2023-07-27 16:53:57 -0500

    Added more stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,8 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
-import { Button } from "./Button";
+import { action } from "@storybook/addon-actions";
 
 import "../tailwind.css";
+
+import Button from "./Button";
+
+import { MdOutlineHome } from "react-icons/md";
 
 const meta = {
   title: "MinUI Components/Button",
@@ -17,15 +20,166 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Primary_Dark: Story = {
   args: {
-    enabled: true,
-    label: "Button",
+    variant: "primary",
+    theme: "dark",
+    children: "Button",
   },
 };
 
-export const Secondary: Story = {
+export const Primary_Light: Story = {
   args: {
-    label: "Button",
+    variant: "primary",
+    theme: "light",
+    children: "Button",
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: "red", value: "#f00" },
+        { name: "green", value: "#0f0" },
+        { name: "blue", value: "#00f" },
+      ],
+    },
+  },
+};
+
+export const Secondary_Dark: Story = {
+  args: {
+    variant: "secondary",
+    theme: "dark",
+    children: "Button",
+  },
+};
+
+export const Secondary_Light: Story = {
+  args: {
+    variant: "secondary",
+    theme: "light",
+    children: "Button",
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: "red", value: "#f00" },
+        { name: "green", value: "#0f0" },
+        { name: "blue", value: "#00f" },
+      ],
+    },
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    variant: "danger",
+    theme: "dark",
+    children: "Button",
+  },
+};
+
+export const Outline_Dark: Story = {
+  args: {
+    variant: "outline",
+    theme: "dark",
+    children: "Button",
+  },
+};
+
+export const Outline_Light: Story = {
+  args: {
+    variant: "outline",
+    theme: "light",
+    children: "Button",
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: "red", value: "#f00" },
+        { name: "green", value: "#0f0" },
+        { name: "blue", value: "#00f" },
+      ],
+    },
+  },
+};
+
+export const Ghost_Dark: Story = {
+  args: {
+    variant: "ghost",
+    theme: "dark",
+    children: "Button",
+  },
+};
+
+export const Ghost_Light: Story = {
+  args: {
+    variant: "ghost",
+    theme: "light",
+    children: "Button",
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: "red", value: "#f00" },
+        { name: "green", value: "#0f0" },
+        { name: "blue", value: "#00f" },
+      ],
+    },
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "small",
+    theme: "dark",
+    children: "Small Button",
+  },
+};
+
+export const Normal: Story = {
+  args: {
+    size: "normal",
+    theme: "dark",
+    children: "Normal Button",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "large",
+    theme: "dark",
+    children: "Large Button",
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    size: "icon",
+    theme: "dark",
+    children: <MdOutlineHome />,
+  },
+};
+
+export const Icon_With_Text: Story = {
+  args: {
+    size: "icon",
+    theme: "dark",
+    children: [<MdOutlineHome className="mr-2" />, `Home`],
+  },
+};
+
+export const Non_Rounded: Story = {
+  args: {
+    rounded: false,
+    theme: "dark",
+    children: "Button",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    theme: "dark",
+    children: "Button",
   },
 };
